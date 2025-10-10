85c58d2daaf27838751dc819d453f151433e119f
bgoodman-ui <bgoodma9@ut.utm.edu>
2025-10-10 16:49:06 +0000

    A bit more to hello

--- GeeksExamples/HelloWorld.cpp
+++ GeeksExamples/HelloWorld.cpp
@@ -2,5 +2,19 @@
 
 int main()
 {
+    //cout Hello World
     std::cout<<"Hello World"<<std::endl;
+    //cout hello world 10 times
+    for (int i = 0; i<10;i++)
+    {
+        std::cout<<"Hello World "<<i+1<<std::endl;
+    }
+    //ask user how many times to say hello world
+    int n;
+    std::cout<<"Say Hello World, how many times ";
+    std::cin>>n;
+    for(int j = 0; j < n; j++)
+    {
+        std::cout<<"Hello World "<<j+1<<std::endl;
+    }
 }
